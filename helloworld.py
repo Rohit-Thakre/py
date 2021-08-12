3b2527c851ee850d1e7ccdce23005cbb4a998a0c
Rohit Thakre <thake785@gmail.com>
2021-08-12 16:17:54 +0530

    helloworld modified.

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,9 @@
 import os
 os.system('cls')
 os.system('color a')
-print("hello world")
+print("printing hello world 50 times, \nfrom 0 to 49")
+for x in range(0, 50):
+    if x < 10:
+        print(x, " hello world.")
+    else:
+        print(x, "hello world.")
