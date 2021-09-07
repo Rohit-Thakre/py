889d8f9aaef91651a71e73fea70cb1e3e8b98989
Rohit Thakre <thake785@gmail.com>
2021-09-07 12:28:20 -0700

    adding files

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,61 +1,97 @@
 
-"""binary search program in python """
-import math
-lst=[0,1,2,3,4,5,6,7,8,9]
-choice = 1
+# """binary search program in python """
+# import math
+# lst=[0,1,2,3,4,5,6,7,8,9]
+# choice = 1
 
-print(lst)
-while True: 
-    print("\nwhat do you want to do ?\n1. Enter element in list\n2.search element\n3.print list.\n4.Exit\n")
-    choice = int(input("\nEnter your choice : "))
+# print(lst)
+# while True: 
+#     print("\nwhat do you want to do ?\n1. Enter element in list\n2.search element\n3.print list.\n4.Exit\n")
+#     choice = int(input("\nEnter your choice : "))
 
-    if choice ==1:
-        element = int(input("Enter your element here : "))
-        lst.append(element)
+#     if choice ==1:
+#         element = int(input("Enter your element here : "))
+#         lst.append(element)
     
-    elif choice ==2:
-    # import math 
-        lst.sort()
-        print("sorted list :", lst)
-        find = int(input("\nEnter the element to search in list/ array: "))
-        mid = len(lst) /2
-        mid = int(math.ceil(mid)) 
-        print("first element of lst", lst[0])
-        for x in range(len(lst)):
-            if lst[mid-1] < find: 
-                new_lst = lst[mid:]
-                new_mid = len(new_lst)/ 2 
-                new_mid = math.ceil(new_mid)
-                mid = mid + new_mid +1
-                if mid >= len(lst):
-                    break
-            elif lst[mid -1] > find: 
-                new_lst = lst[:mid-1]
-                new_mid = len(new_lst) / 2 
-                new_mid =math.ceil(new_mid)
-                mid = mid - new_mid 
-                if mid <= 0:
-                    break
+#     elif choice ==2:
+#     # import math 
+#         lst.sort()
+#         print("sorted list :", lst)
+#         find = int(input("\nEnter the element to search in list/ array: "))
+#         mid = len(lst) /2
+#         mid = int(math.ceil(mid)) 
+#         count = mid
+#         print('mid :',mid)
+#         for x in lst:
+#             if find == lst[mid] :
+#                 break
+#             elif lst[mid] < find: 
+#                 lst = lst[mid:]
+#                 mid = len(lst)/ 2 
+#                 mid = math.ceil(mid)
+#                 count = mid + count
+                
+#                 # mid = mid + new_mid 
+#                 if mid >= len(lst):
+#                     break
+#             elif lst[mid] > find: 
+#                 lst = lst[:mid]
+#                 mid = len(lst) / 2 
+#                 mid =math.ceil(mid)
+#                 count = count - mid
+#                 # mid = mid - new_mid 
+#                 if mid <= 0:
+#                     break
+#             else:
+#                 break
 
-            elif find == lst[mid-1] :
-                break
-            else:
-                break
-        if lst [mid -1] == find:
-            print("\nsearch was done successfully !")
-            print("\nThe element you are looking for is found at position :", mid)
-        else: 
-            print("\nThe search was unsuccessful \n")
-            print(f"element {find} not found in the given list/array")
 
-    elif choice == 3:
-        lst.sort()
-        print("The list is:\n")
-        for index , item in enumerate(lst, start = 0):
-            print(index, item)
+#         if lst [mid] == find:
+#             print("\nsearch was done successfully !")
+#             print("\nThe element you are looking for is found at position :", count)
+#         else: 
+#             print("\nThe search was unsuccessful \n")
+#             print(f"element {find} not found in the given list/array")
+
+#     elif choice == 3:
+#         lst.sort()
+#         print("The list is:\n")
+#         for index , item in enumerate(lst, start = 0):
+#             print(index, item)
     
-    elif choice == 4: 
-        exit()
+#     elif choice == 4: 
+#         exit()
 
 
  
+
+
+import math 
+lst = [1,2,3,4,5,6,7,8,9,10]
+find = int(input("enter your no. search : "))
+
+
+count = 0
+
+for x in lst: 
+    mid = len(lst)/2
+    mid = math.ceil(mid)
+    count = mid
+    print("\n")
+    # print('first mid is : ', mid)
+
+    if lst[mid] ==find: 
+        print("got it at ",count)
+        break
+    
+    elif lst[mid] <find: 
+        lst = lst[mid:]
+        count = count - mid
+        print('mid:',mid, 'new_lst:', lst, 'count :',count)
+
+    
+    elif lst[mid] > find: 
+        lst = lst[:mid]
+        count = count +mid
+        print('mid:',mid, 'new_lst',lst,'count :', count)
+    
--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,1 @@
-import os
-os.system('cls')
-os.system('color a')
-print("printing hello world 50 times, \nfrom 0 to 49")
-for x in range(0, 50):
-    if x < 10:
-        print(x, " hello world.")
-    else:
-        print(x, "hello world.")
+test
