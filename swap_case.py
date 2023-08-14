b62307020f5b18a9f9100b7e20e530cfc50142a2
Rohit Thakre <thake785@gmail.com>
2023-08-14 20:41:56 +0530

    new header added.

--- swap_case.py
+++ swap_case.py
@@ -1,10 +1,11 @@
+# swap the cases 
 def swap_case(txt):
     txt = txt.swapcase()
     return txt
 
 if __name__ == '__main__' :
     text = str(input("Enter your text here")) 
-    # Hello World ---> hELLO wORLD
+    # Hello World ---> hELLO wORLD 
     result = swap_case(text)
     print("input text: ", text)
     print("swap  text: ",result)
